fe1290f2e18ca2799199d4535ff18279515ccbd4
Md shamim sorkar <61225746+error-19@users.noreply.github.com>
2023-11-27 13:37:18 +0600

    TestNG

---
+++ src/test/java/com/hrmanagelive/BaseClass.java
@@ -0,0 +1,1 @@
+package com.hrmanagelive;import org.openqa.selenium.WebDriver;public class BaseClass {    public static WebDriver driver;    public String Baseurl="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";}
